b1c3228242a73d57e843fa10d7ca357e1c1b5031
Abhishek Samariya <asamariya@gmail.com>
2020-02-15 16:12:55 +1100

    Added MainHeader

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 
 import './UsersList.scss';
 
@@ -8,13 +9,15 @@ const UsersList = ({ items }) => {
   if (items.length === 0) {
     return (
       <div className="text-center">
-        <h2>No users found</h2>
+        <Card>
+          <h2>No users found</h2>
+        </Card>
       </div>
     );
   }
 
   return (
-    <ul>
+    <ul className="users-list">
       {items.map(user => {
         const { id, name, image, places } = user;
         return (
