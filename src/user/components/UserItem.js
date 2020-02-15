525c0e79ec4e7d5f2289d22db88b515e8d633447
Abhishek Samariya <asamariya@gmail.com>
2020-02-15 15:50:22 +1100

    Basic styling for UserItem, to be improved upon later

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Avatar from '../../shared/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 
 import './UserItem.scss';
 
 const UserItem = ({ id, name, image, placeCount }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h4>
+              {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
+            </h4>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
