35423e0252a8d025c31a9035347d2a7673c709d8
Abhishek Samariya <asamariya@gmail.com>
2020-02-15 15:09:45 +1100

    Setup users and new place routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
-    <div className="App mt-5">
-      <h1>App.js</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Users />
+        </Route>
+        <Route exact path="/places/new">
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 };
 
