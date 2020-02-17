3ddc0c3542c314a6c90da8322248040aed05b8c2
Abhishek Samariya <asamariya@gmail.com>
2020-02-18 06:11:27 +1100

    Implement Google Maps in Map.js

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 import './PlaceItem.scss';
 
 const PlaceItem = ({
@@ -29,7 +30,7 @@ const PlaceItem = ({
         footer={<Button onClick={closeMap}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!</h2>
+          <Map center={coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
