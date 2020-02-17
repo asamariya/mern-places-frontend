a021a65d539696366b4bca88197f74a533b6e96f
Abhishek Samariya <asamariya@gmail.com>
2020-02-18 06:50:59 +1100

    Manage state in the Input component using useReducer

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,13 @@ import Input from '../../shared/components/FormElements/Input';
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.scss';
 
-const Input = ({ element, id, type, placeholder, rows, label }) => {
+const initialState = {
+  value: '',
+  isValid: false
+};
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = ({ element, id, type, placeholder, rows, label, errorText }) => {
+  const [inputState, dispatch] = useReducer(inputReducer, initialState);
+
+  const changeHandler = e => {
+    dispatch({ type: 'CHANGE', val: e.target.value });
+  };
   const inputElement =
     element === 'input' ? (
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={id} rows={rows || 3} />
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     );
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${!inputState.isValid &&
+        'form-control--invalid'}`}
+    >
       <label htmlFor={id}>{label}</label>
       {inputElement}
+      {!inputState.isValid && <p>{errorText}</p>}
     </div>
   );
 };
