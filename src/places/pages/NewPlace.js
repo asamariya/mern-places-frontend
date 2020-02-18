e26416cb88d0c0309b8e1cbc12adf44753f77d4b
Abhishek Samariya <asamariya@gmail.com>
2020-02-18 14:22:30 +1100

    Fixed description label

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -22,7 +22,7 @@ const NewPlace = () => {
       />
       <Input
         id="description"
-        label="Title"
+        label="Description"
         element="textarea"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
