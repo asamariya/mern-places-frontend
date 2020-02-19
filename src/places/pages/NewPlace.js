1036dc87281c74abc0573da8ce9f79090a7b8e26
Abhishek Samariya <asamariya@gmail.com>
2020-02-19 17:40:01 +1100

    Added address input to the add place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -61,8 +61,13 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = e => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         type="text"
@@ -78,6 +83,14 @@ const NewPlace = () => {
         element="textarea"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        label="Address"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
