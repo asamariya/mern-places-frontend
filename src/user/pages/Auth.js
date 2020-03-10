248a39346eb6a0880c69664b68e2b1a6b553d925
Abhishek Samariya <asamariya@gmail.com>
2020-03-10 18:38:30 +1100

    Updated signInHandler in Auth.js to send a POST request to the backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -33,9 +33,30 @@ const Auth = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [formState, inputHandler, setFormData] = useForm(initialState);
 
-  const signInHandler = e => {
+  const signInHandler = async e => {
     e.preventDefault();
-    console.log(formState.inputs);
+
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     auth.login();
   };
 
